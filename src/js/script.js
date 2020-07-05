38871e0400a4fd497a2ab052aa62872685faf360
Irena <irenasovko@mail.ru>
2020-07-05 17:32:52 +0300

    js

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow:
+      });
+  });
+      
