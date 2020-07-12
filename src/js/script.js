132346e8fe1285ca59f41ef6bd71518621e83bfa
Irena <irenasovko@mail.ru>
2020-07-12 23:03:40 +0300

    media

--- src/js/script.js
+++ src/js/script.js
@@ -16,7 +16,7 @@
       });
   });
        */ 
-
+/* 
       const slider = tns({
         container: '.carousel__inner',
         items: 1,
@@ -32,5 +32,23 @@
       });
 
       document.querySelector('.next').addEventListener ('click', function () {
+        slider.goTo('next');
+      }); */
+
+      const slider = tns({
+        container: '.carousel__item',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        controls: false,
+        nav: false
+  
+      });
+
+      document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+      });
+
+      document.querySelector('.next').addEventListener('click', function () {
         slider.goTo('next');
       });
